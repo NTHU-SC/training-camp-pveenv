5d83dbcfcaa86901ce32d804ff72ba9db558f685
牟展佑 <william.mou1024@gmail.com>
2021-01-22 20:27:36 +0800

    Update create-sc.sh

--- create-sc.sh
+++ create-sc.sh
@@ -4,8 +4,8 @@ do
 	vmi=$(printf "%02d" $i);	
 	qm clone ${1}1 21${vmi}0 --name scteam${vmi}-head && \
 	qm set ${year}${vmi}0   --autostart 1 \
-				--net1 model=virtio,trunks=21${vmi}
+				--net1 model=virtio,tag=21${vmi}
 	qm clone ${1}2 21${vmi}1 --name scteam${vmi}-work && \
 	qm set ${year}${vmi}1   --autostart 1 \
-				--net0 model=virtio,trunks=21${vmi}
+				--net0 model=virtio,tag=21${vmi}
 done
